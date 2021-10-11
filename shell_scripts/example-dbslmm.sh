d45dec30c8d0d17a64eb88b6def71e8e740d32f6
Frederick J. Boehm <frederick.boehm@gmail.com>
2021-10-11 12:56:49 -0400

    switched model type in shell script

--- shell_scripts/example-dbslmm.sh
+++ shell_scripts/example-dbslmm.sh
@@ -15,7 +15,7 @@ nobs=`sed -n "2p" ${summf}${chr}.assoc.txt | awk '{print $5}'`
 nmis=`sed -n "2p" ${summf}${chr}.assoc.txt | awk '{print $4}'`
 n=$(echo "${nobs}+${nmis}" | bc -l)
 ## execute Rscript
-Rscript ${DBSLMM} --summary ${summf}${chr}.assoc.txt --outPath ${outPath} --plink ${plink} --dbslmm ${dbslmm} --ref ${ref}${chr} --n ${n} --nsnp ${m} --type d --model LMM --block ${blockf}${chr}.bed --h2 ${h2}
+Rscript ${DBSLMM} --summary ${summf}${chr}.assoc.txt --outPath ${outPath} --plink ${plink} --dbslmm ${dbslmm} --ref ${ref}${chr} --n ${n} --nsnp ${m} --type d --model DBSLMM --block ${blockf}${chr}.bed --h2 ${h2}
 ### Predict
 bfilete=../../DBSLMM/test_dat/test_chr
 est=../../DBSLMM/test_dat/out/summary_gemma_chr
